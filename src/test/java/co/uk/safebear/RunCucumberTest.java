348c59fbea1f400932a513201d239e14ffc5d38c
student <student@safebear.co.uk>
2019-07-22 15:45:56 +0100

    Commit login 2

--- src/test/java/co/uk/safebear/RunCucumberTest.java
+++ src/test/java/co/uk/safebear/RunCucumberTest.java
@@ -1,10 +1,17 @@
 package co.uk.safebear;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(
+plugin = {"json;target/cucumber-report/cucumber.json"} ,
+        tags = "not@to-do"
+
+        //glue = "path to stepdefs if not in the same folder"
+        //Features = "path to features if not in co.uk.safebear folder"
+)
 public class RunCucumberTest {
+
 }
